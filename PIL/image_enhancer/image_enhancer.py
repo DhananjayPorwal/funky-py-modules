85f8b1fa824a2631c44b16de99bba4eb7e1d4d8f
Dhananjay Porwal <51775507+DhananjayPorwal@users.noreply.github.com>
2021-09-06 10:36:37 +0530

    Update image_enhancer.py

--- PIL/image_enhancer/image_enhancer.py
+++ PIL/image_enhancer/image_enhancer.py
@@ -1,17 +1,17 @@
 from PIL import Image, ImageEnhance
 
-im = Image.open("image.png")
+im = Image.open("image.jpeg")
 
 enhancer = ImageEnhance.Contrast(im)
 
 factor = 1 #gives original image
 im_output = enhancer.enhance(factor)
-im_output.save('original-image.png')
+im_output.save('original-image.jpeg')
 
 factor = 0.5 #decrease constrast
 im_output = enhancer.enhance(factor)
-im_output.save('less-contrast-image.png')
+im_output.save('less-contrast-image.jpeg')
 
 factor = 1.5 #increase contrast
 im_output = enhancer.enhance(factor)
-im_output.save('more-contrast-image.png')
+im_output.save('more-contrast-image.jpeg')
